952375a085a9459ce4a37d881d9ef3e7f114361e
Francois ROLAND <froland@froland.be>
2020-11-22 17:52:27 +0100

    Implement book form.

--- public/scripts/book-script.js
+++ public/scripts/book-script.js
@@ -45,4 +45,66 @@ function loadBooks() {
     });
 }
 
+const bookForm = document.getElementById('book-form');
+const authorInput = document.getElementById('authorInput');
+const titleInput = document.getElementById('titleInput');
+const yearInput = document.getElementById('yearInput');
+const sendingFailure = document.getElementById('sending-failure');
+
+function sendForm() {
+  authorInput.setAttribute('disabled', true);
+  titleInput.setAttribute('disabled', true);
+  yearInput.setAttribute('disabled', true);
+
+  const data = {
+    author: authorInput.value,
+    title: titleInput.value,
+    year: yearInput.value,
+  };
+
+  const abortController = new AbortController();
+  const timer = setTimeout(() => {
+    abortController.abort();
+  }, REQUEST_TIMEOUT);
+
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  headers.append('Accept', 'application/json');
+
+  fetch('/api/books', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers,
+    signal: abortController.signal,
+  })
+    .then((response) => {
+      if (response.ok) {
+        loadBooks();
+        bookForm.reset();
+        bookForm.classList.remove('was-validated');
+        window.scrollTo(0, 0);
+      }
+    })
+    .catch((error) => {
+      sendingFailure.classList.remove('d-none');
+    })
+    .finally(() => {
+      authorInput.removeAttribute('disabled');
+      titleInput.removeAttribute('disabled');
+      yearInput.removeAttribute('disabled');
+      clearTimeout(timer);
+    });
+}
+
+bookForm.addEventListener('submit', (event) => {
+  sendingFailure.classList.add('d-none');
+  event.preventDefault();
+  event.stopPropagation();
+  if (bookForm.checkValidity()) {
+    sendForm();
+  }
+
+  bookForm.classList.add('was-validated');
+});
+
 loadBooks();
